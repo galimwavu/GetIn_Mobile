4e5f183bb4c9cd052f7c8df16e4a870e5dadb5e3
Martin <galimwavu@users.noreply.github.com>
2016-09-07 07:48:01 +0300

    Update PatientParcel.java
    included more writes and reads

--- api-android/src/main/java/org/sana/android/content/core/PatientParcel.java
+++ api-android/src/main/java/org/sana/android/content/core/PatientParcel.java
@@ -55,34 +55,37 @@ public class PatientParcel extends Patient implements Parcelable {
 			setCreated(DateUtil.parseDate(in.readString()));
 			setModified(DateUtil.parseDate(in.readString()));
 			setDob(DateUtil.parseDate(in.readString()));
+			setLMD(DateUtil.parseDate(in.readString()));
 		} catch (ParseException e) {			
 			e.printStackTrace();
 			throw new IllegalArgumentException(e);
 		}
 		setGiven_name(in.readString());
-		setSurname(in.readString());
-		setDob(in.readInt());
-		setPhone_number(in.readString());
-		setPowerholder_phone_(in.readString());
-		setLocation(in.readString());
-		setLast_menstruation_date(in.readString());
+		setFamily_name(in.readString());
+		setGender(in.readString());
+		setpNumber(in.readString());
+
+		setHolder_pNumber(in.readString());
+//		setLMD(in.read());
 		setMarital_status(in.readString());
 		setEducation_level(in.readString());
 		setContraceptive_use(in.readString());
 		setANC_status(in.readString());
-		setNext_ANC_visit(in.readString());
-		setExpected_date_of_delivery(in.readString());
-		setSMS_intrest(in.readString());
-		//setFollow_up(in.readString());
+		setANC_visit(in.readString());
+		setEDD(in.readString());
+		setreceive_sms(in.readString());
+		setfollow_up(in.readString());
 		setCUG_status(in.readString());
-		setGeneral_comment(in.readString());
-		
-		//setGender(in.readString());
-		//setImage(URI.create(in.readString()));
-        //setSystemId(in.readString());
-		//setConfirmed((in.readInt() == 1));
-	//	setDobEstimated((in.readInt() == 1));
-	
+		setcomment(in.readString());
+		setBleeding(in.readString());
+		setFever(in.readString());
+		setSwollen_feet(in.readString());
+		setBlurred_vision(in.readString());
+
+		setImage(URI.create(in.readString()));
+        setSystemId(in.readString());
+		setConfirmed((in.readInt() == 1));
+		setDobEstimated((in.readInt() == 1));
 	}
 
     /**
@@ -97,26 +100,31 @@ public class PatientParcel extends Patient implements Parcelable {
         setModified(patient.getModified());
         setDob(patient.getDob());
         setGiven_name(patient.getGiven_name());
-        setSurname(patient.getSurname());
-        //setGender(patient.getGender());
-        //setImage(patient.getImage());
-        //setSystemId(patient.getSystemId());
-		//setConfirmed(patient.getConfirmed());
-		//setDobEstimated(patient.isDobEstimated());
-		setPhone_number(patient.getPhone_number());
-		setPowerholder_phone_number(patient.getPowerholder_phone_number());
-		setLocation(patient.getLocation());
-		setLast_menstruation_date(patient.getLast_menstruation_date());
+        setFamily_name(patient.getFamily_name());
+        setGender(patient.getGender());
+		setpNumber(patient.getpNumber());
+
+		setHolder_pNumber(patient.getHolder_pNumber());
+		setLMD(patient.getLMD());
 		setMarital_status(patient.getMarital_status());
 		setEducation_level(patient.getEducation_level());
 		setContraceptive_use(patient.getContraceptive_use());
 		setANC_status(patient.getANC_status());
-		setNext_ANC_visit(patient.getANC_visit());
-		setExpected_date_of_delivery(patient.getExpected_date_of_delivery());
-		setSMS-intrest(patient.getSMS_intrest());
-		//setFollow_up(patient.getFollow_up());
+		setANC_visit(patient.getANC_visit());
+		setEDD(patient.getEDD());
+		setreceive_sms(patient.getReceive_sms());
+		setfollow_up(patient.getFollow_up());
 		setCUG_status(patient.getCUG_status());
-		setGeneral_comment(patient.getGeneral_comment());
+		setcomment(patient.getComment());
+		setBleeding(patient.getBleeding());
+		setFever(patient.getFever());
+		setSwollen_feet(patient.getSwollen_feet());
+		setBlurred_vision(patient.getBlurred_vision());
+
+        setImage(patient.getImage());
+        setSystemId(patient.getSystemId());
+		setConfirmed(patient.getConfirmed());
+		setDobEstimated(patient.isDobEstimated());
     }
 
     public PatientParcel(){}
@@ -139,29 +147,30 @@ public class PatientParcel extends Patient implements Parcelable {
 		dest.writeString(DateUtil.format(getModified()));
 		dest.writeString(DateUtil.format(getDob()));
 		dest.writeString(getGiven_name());
-		dest.writeString(getSurname());
-		dest.writeString(getPhone_number());
-		dest.writeString(getPowerholder_phone_number());
-		dest.writeString(getLocation());
-		dest.writeString(getLast_menstruation_date());
+		dest.writeString(getFamily_name());
+		dest.writeString(getGender());
+
+		dest.writeString(getHolder_pNumber());
+		dest.writeString(DateUtil.format(getLMD()));
 		dest.writeString(getMarital_status());
 		dest.writeString(getEducation_level());
-		dest.writeString(getContraceptive_use);
-		dest.writeString(getNext_ANC_status());
-		dest.writeString(getNext_ANC_visit());
-		dest.writeString(getExpected_date_of_delivery());
-		dest.writeString(getSMS_intrest());
-		//dest.writeString(getFollow_up());
+		dest.writeString(getContraceptive_use());
+		dest.writeString(getANC_status());
+		dest.writeString(getANC_visit());
+		dest.writeString(getEDD());
+		dest.writeString(getReceive_sms());
+		dest.writeString(getFollow_up());
 		dest.writeString(getCUG_status());
-		dest.writeString(getGeneral_comment());
-		
-		
-		/*dest.writeString(getGender());
+		dest.writeString(getComment());
+		dest.writeString(getBleeding());
+		dest.writeString(getFever());
+		dest.writeString(getSwollen_feet());
+		dest.writeString(getBlurred_vision());
+
 		dest.writeString(getImage().toString());
         dest.writeString(getSystemId());
 		dest.writeInt((getConfirmed())? 1: 0);
-		dest.writeInt((isDobEstimated())? 1: 0);*/
-		
+		dest.writeInt((isDobEstimated())? 1: 0);
 	}
 	
 	public static final Parcelable.Creator<PatientParcel> CREATOR = 
